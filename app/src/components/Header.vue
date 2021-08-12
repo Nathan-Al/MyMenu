6b77ca64011ab93c7eaf3cfce0f0bf5cb25ffb02
Ehacko <Darkdav.t@gmail.com>
2021-08-12 20:05:58 +0000

    Add vue ui components

---
+++ app/src/components/Header.vue
@@ -0,0 +1,11 @@
+<template lang="pug">
+header header
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'Header',
+});
+</script>
---
+++ app/src/components/ListeRestaurants.vue
@@ -0,0 +1,14 @@
+<template lang="pug">
+//- listeRestaurants
+.liste_restaurants
+    slot
+
+</template>
+
+<script lang="ts">
+  import { defineComponent } from 'vue';
+
+  export default defineComponent({
+    name: 'ListeRestaurants',
+  });
+</script>
