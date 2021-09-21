f319447f61957ce816859ce7d4c385f4fe20e245
allan <47801661+Nathan-Al@users.noreply.github.com>
2021-09-21 13:55:09 -0400

    Add control origin to the index read

--- templates/index/read.php
+++ templates/index/read.php
@@ -1,11 +1,15 @@
 <?php
+// Headers requis
+// Accès depuis n'importe quel site ou appareil (*)
+header('Access-Control-Allow-Origin: *');
+
 // Format des données envoyées
-header("Content-Type: application/json; charset=UTF-8");
+header('Content-Type: application/json; charset=UTF-8');
 
 // Durée de vie de la requête
-header("Access-Control-Max-Age: 3600");
+header('Access-Control-Max-Age: 3600');
 
 // Entêtes autorisées
-header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+header('Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With');
 ?>
 <?= $response; ?>
