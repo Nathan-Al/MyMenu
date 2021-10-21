8b34d6afd95c55af503667243fb04b669a05cfe8
Ehacko <Darkdav.T@gmail.com>
2021-10-20 22:58:45 -0400

    Création des interaces "accueil" et "recherche"

---
+++ app/src/components/BtnFiltreAnnuaire/BtnFiltreAnnuaire.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="btn-filtre-annuaire">
+    <div class="background">
+      <div class="gradient"></div>
+    </div>
+    <div class="titre_app">Par Filter</div>
+  </div>
+</template>
+
+<script lang="ts">
+
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'BtnFiltreAnnuaire'
+})
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+
+</style>
--- app/src/views/Home.vue
+++ app/src/views/Home.vue
@@ -1,19 +1,27 @@
 <template>
-  <div class="home">
-    <!-- <img alt="Vue logo" src="../assets/logo.png"> -->
-    <DropdownMenu/>
+  <div id="accueil" class="vue">
+    <div class="content">
+      <div class="titre_app">Title</div>
+      <SelectSlider>
+        <RestaurantType/>
+      </SelectSlider>
+      <input type="text" placeholder="Your text here">
+      <BtnFiltreAnnuaire/>
+    </div>
   </div>
 </template>
 
 <script lang="ts">
 import { Options, Vue } from 'vue-class-component'
-import HelloWorld from '@/components/HelloWorld.vue' // @ is an alias to /src
-import DropdownMenu from '@/components/DropdownMenu/DropdownMenu.vue'
+import SelectSlider from '@/components/SelectSlider/SelectSlider.vue'
+import RestaurantType from '@/components/RestaurantType/RestaurantType.vue'
+import BtnFiltreAnnuaire from '@/components/BtnFiltreAnnuaire/BtnFiltreAnnuaire.vue'
 
 @Options({
   components: {
-    HelloWorld,
-    DropdownMenu
+    SelectSlider,
+    RestaurantType,
+    BtnFiltreAnnuaire
   }
 })
 export default class Home extends Vue {}
